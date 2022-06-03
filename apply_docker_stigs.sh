2bcb57d627c70ecaf260a083634570d24b825d3f
Matt McClaskey <matt@kasmweb.com>
2022-06-03 14:34:33 -0400

    WIP added more checks

--- apply_docker_stigs.sh
+++ apply_docker_stigs.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 set -e
-set -x
+#set -x
 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
@@ -106,25 +106,13 @@ else
 	echo "V-235812 PASS, no seccomp unconfined containers found"
 fi
 
-set +e
-docker ps --quiet --all | xargs --no-run-if-empty -- docker inspect --format '{{ .Id }}: Ulimits={{ .HostConfig.Ulimits }}' | grep "no value"
-pipe_status=(${PIPESTATUS[@]})
-set -e
-inner_commands=(${pipe_status[@]:0:2})
-grep_rc=${pipe_status[@]:2}
-for rc in ${inner_commands[@]}; do
-	if [ $rc -ne 0 ]; then
-		echo "Inner command in V-235844 check failed, exiting"
-		exit 1
-	fi
-done
 
-#if [ $grep_rc -eq 0 ]; then
-#	echo "V-235844 FAIL, container overrides ulimit"
-#	exit 1
-#else
-#	echo "V-235844 PASS, no containers override default ulimit"
-#fi
+if docker ps --quiet --all | xargs --no-run-if-empty -- docker inspect --format '{{ .Id }}: Ulimits={{ .HostConfig.Ulimits }}' | grep -v "no value" ; then
+    echo "V-235844 FAIL, container overrides ulimit"
+	exit 1
+else
+	echo "V-235844 PASS, no containers override default ulimit"
+fi
 
 # can be configured as docker daemon argument
 if ps -ef | grep dockerd | grep --quiet 'insecure-registry'; then
@@ -181,7 +169,7 @@ else
     if grep '"ip"' /etc/docker/daemon.json ; then
       echo 'V-235820 FAIL, /etc/docker/daemon.json configured with IP set to 0.0.0.0, manually fix and rerun'
     else
-      cat <<< $(sudo jq ". |= . + {\"IP\": \"$PRI_IP\"}" /etc/docker/daemon.json) > /etc/docker/daemon.json
+      cat <<< $(sudo jq ". |= . + {\"ip\": \"$PRI_IP\"}" /etc/docker/daemon.json) > /etc/docker/daemon.json
       echo "V-235820 PASS, docker has been bound to $PRI_IP, be sure to restart the docker service."
 	fi
   else
@@ -198,3 +186,126 @@ else
   echo 'V-235799 PASS, all containers running with apparmor profiles'
 fi
 
+# --------------------------Matt---------------------------------------------
+
+docker ps -q | xargs docker inspect --format '{{ .Id }}: {{ .Name }}: Ports={{ .NetworkSettings.Ports }}' | grep HostPort
+echo 'V-235837 MANUAL, review above ports and ensure they are in the SSP, look at the HostPort field.'
+
+docker ps --quiet | xargs docker inspect --format '{{ .Id }}: Ports={{ .NetworkSettings.Ports }}' | grep -i host
+echo 'V-235804 MANUAL, review above ports and ensure they are in the SSP, look at the HostPort field.'
+
+if sudo ausearch -k docker | grep exec | grep --quiet privileged ; then
+  echo 'V-235813 FAIL, there is an exec session running with privileged flag'
+  exit 1
+else
+  echo 'V-235813 PASS, no exec sessions with privilged flag found'
+fi
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: UsernsMode={{ .HostConfig.UsernsMode }}' | grep --quiet -i "UsernsMode=host" ; then
+  echo 'V-235817 FAIL, containers present sharing host user namespace'
+  exit 1
+else
+  echo 'V-235817 PASS, no containers running sharing host user namespace detected'
+fi
+
+LOW_HOST_PORT=$(docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: Ports={{ .NetworkSettings.Ports }}' | grep -Pio '(?<=HostPort:)\d+' | sort -n | head -n 1)
+if [ "$LOW_HOST_PORT" -lt 1024 ] ; then 
+    echo 'V-235819 FAIL host ports below 1024 are mapped into containers.'; 
+	exit 1
+else 
+	echo 'V-235819 PASS, no host ports mapped below 1024'; 
+fi
+
+if docker ps --all | grep -iv "ucp\|kube\|dtr" | awk '{print $1}' | xargs docker inspect --format '{{ .Id }}: NetworkMode={{ .HostConfig.NetworkMode }}' | grep --quiet -i "NetworkMode=host" ; then
+  echo 'V-235805 FAIL, containers present sharing hosts network namespace'
+  exit 1
+else
+  echo 'V-235805 PASS, no containers running sharing hosts netork namespace'
+fi
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: Devices={{ .HostConfig.Devices }}' | grep --quiet -i 'pathincontainer' ; then
+  echo 'V-235809 FAIL, containers present with host devices passed in.'
+  exit 1
+else
+  echo 'V-235809 PASS, no containers running with host devices passed in.'
+fi 
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: Volumes={{ .Mounts }}' | grep -iv "ucp\|kubelet\|dtr" | grep -Po 'Source:\S+' | grep -P '\:(/|/boot|/dev|/etc|/lib|/proc|/sys|/usr)$' ; then
+  echo 'V-235783 FAIL, sensitive directories mapped into containers detected.'
+  exit 1
+else
+  echo 'V-235783 PASS, no sensitive directories found mappend into containers'
+fi 
+
+if docker info | grep --quiet -e "^Storage Driver:\s*aufs\s*$" ; then
+  echo 'V-235790 FAIL, aufs file system detected.'
+  exit 1
+else
+  echo 'V-235790 PASS, aufs file system not detected'
+fi 
+
+if docker ps --all | grep -iv "ucp\|kube\|dtr" | awk '{print $1}' | xargs docker inspect --format '{{ .Id }}: Propagation={{range $mnt := .Mounts}} {{json $mnt.Propagation}} {{end}}' | grep --quiet 'shared' ; then
+  echo 'V-235810 FAIL, mount progagation mode set to shared.'
+  exit 1
+else
+  echo 'V-235810 PASS, no mounts set to shared propogation mode found'
+fi 
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: UTSMode={{ .HostConfig.UTSMode }}' | grep -i '=host' ; then
+  echo 'V-235811 FAIL, host UTS namespace shared to container.'
+  exit 1
+else
+  echo 'V-235811 PASS, no containers found with host UTC namespace shared'
+fi
+
+if ps aux | grep 'docker exec' | grep '\-\-user' ; then
+  echo 'V-235814 FAIL, there is an exec session running with user flag'
+  exit 1
+else
+  echo 'V-235814 PASS, no exec sessions with user flag found'
+fi
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: CgroupParent={{ .HostConfig.CgroupParent }}' | grep -P '=\w+' ; then
+  echo 'V-235815 FAIL, cgroup usage detected, must be manually checked.'
+  exit 1
+else
+  echo 'V-235815 PASS, only default cgroups defined on running containers'
+fi
+
+if docker ps --quiet --all | grep -iv "ucp\|kube\|dtr" | awk '{print $1}' | xargs docker inspect --format '{{ .Id }}: Privileged={{ .HostConfig.Privileged }}' | grep true ; then
+  echo 'V-235802 FAIL, containers running as privileged.'
+  exit 1
+else
+  echo 'V-235802 PASS, no containers found running as privileged'
+fi
+
+if which auditctl ; then
+  if !(systemctl show -p FragmentPath docker.service or auditctl -l | grep docker.service) then
+    echo 'V-235779 FAIL, docker.service auditd rule missing'
+	exit 1
+  fi
+  if !(systemctl show -p FragmentPath docker.socket or auditctl -l | grep docker.sock) then
+    echo 'V-235779 FAIL, docker.docket auditd rule missing'
+	exit 1
+  fi
+  echo 'V-235779 FAIL, Required auditd rules for docker are present'
+else
+  echo 'V-235779 FAIL, auditd does not appear to be installed, which will result in many STIG findings'
+fi
+
+if docker ps --quiet --all | xargs docker inspect --format '{{ .Id }}: CapAdd={{ .HostConfig.CapAdd }} CapDrop={{ .HostConfig.CapDrop }}' | grep -v ': CapAdd=<no value> CapDrop=<no value>$' ; then
+  echo 'V-235801 FAIL, containers running with added capabilities, you will need to manually confirm with SSP.'
+  exit 1
+else
+  echo 'V-235801 PASS, no containers found with additional capabilities passed in.'
+fi
+
+for i in $(docker ps -qa); do 
+  if docker exec $i ps -el | grep -i sshd ; then
+    echo 'V-235803 FAIL, containers running sshd found.'
+    exit 1
+  fi
+done
+echo 'V-235803 PASS, no containers running sshd found.'  
+
+#----------------------- end matt --------------------------------------------
