e9a736f30c6a5ace923745a6d0615d546671f214
arpit gupta <arpit.gupta@kasmweb.com>
2025-09-23 15:53:58 +0000

    KASM--7762 minimize changes

--- apply_kasm_stigs.sh
+++ apply_kasm_stigs.sh
@@ -151,13 +151,12 @@ if [[ -n "${SHOW_ARTIFACT}" ]]; then
     echo "Output: $("${YQ_BIN}" -e '.services[].deploy.resources.limits' /opt/kasm/current/docker/docker-compose.yaml )"
 fi
 
-# Set restart policy for service containers V-235843 (STIG-compliant)
+# Set restart policy for service containers V-235843
 if "${YQ_BIN}" -e '.services[].restart' /opt/kasm/current/docker/docker-compose.yaml > /dev/null 2>&1; then
     # Baseline policy for all services
-    "${YQ_BIN}" -i 'del(.services[].restart) | .services.[] *= {
-        "deploy": {"restart_policy": {"condition": "on-failure", "delay": "15s", "max_attempts": 5, "window": "60s"}}
-    }' /opt/kasm/current/docker/docker-compose.yaml
-    
+    "${YQ_BIN}" -i 'del(.services[].restart) | .services.[] *= {"deploy": {"restart_policy": {"condition": "on-failure", "delay": "15s", "max_attempts": 5, "window": "60s"}}}' 
+    /opt/kasm/current/docker/docker-compose.yaml
+
     RESTART_CONTAINERS="true"
     log_succes "V-235843" "restart limits have been set on containers"
 else
