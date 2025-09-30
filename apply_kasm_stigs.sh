72f64e665d2de22e718daf18e70b58a0ed5c0266
Adam Beardwood <adam.beardwood@kasmweb.com>
2025-09-30 13:00:01 +0100

    Increase pid limits to 1000

--- apply_kasm_stigs.sh
+++ apply_kasm_stigs.sh
@@ -195,8 +195,8 @@ fi
 
 # Set pid limits for all containers V-235828
 if ! "${YQ_BIN}" -e '.services[].pids_limit' /opt/kasm/current/docker/docker-compose.yaml > /dev/null 2>&1; then
-    "${YQ_BIN}" -i '.services.[] += {"pids_limit": 100}' /opt/kasm/current/docker/docker-compose.yaml
-    "${YQ_BIN}" -i '.services.[].deploy.resources.limits += {"pids": 100}' /opt/kasm/current/docker/docker-compose.yaml
+    "${YQ_BIN}" -i '.services.[] += {"pids_limit": 1000}' /opt/kasm/current/docker/docker-compose.yaml
+    "${YQ_BIN}" -i '.services.[].deploy.resources.limits += {"pids": 1000}' /opt/kasm/current/docker/docker-compose.yaml
     if "${YQ_BIN}" -e '.services.kasm_guac' /opt/kasm/current/docker/docker-compose.yaml > /dev/null 2>&1; then
         "${YQ_BIN}" -i '.services.kasm_guac += {"pids_limit": 1000}' /opt/kasm/current/docker/docker-compose.yaml
         "${YQ_BIN}" -i '.services.kasm_guac.deploy.resources.limits += {"pids": 1000}' /opt/kasm/current/docker/docker-compose.yaml
